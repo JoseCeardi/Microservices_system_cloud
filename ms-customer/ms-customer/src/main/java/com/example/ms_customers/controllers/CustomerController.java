b089daccc1219e5f0e6eaae29f8a9d41370cdc8f
Joseardi <jose.ceardi@usach.cl>
2026-03-13 19:39:14 -0300

    aply configs for ms-customer with dinamic ports

--- ms-customer/ms-customer/src/main/java/com/example/ms_customers/controllers/CustomerController.java
+++ ms-customer/ms-customer/src/main/java/com/example/ms_customers/controllers/CustomerController.java
@@ -6,6 +6,7 @@ import com.example.ms_customers.services.CustomerService;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,6 +19,7 @@ public class CustomerController {
     private CustomerRepository customerRepository;
     private CustomerService customerService;
 
+    @GetMapping
     public ResponseEntity<List<CustomerDTO>> getAllCustomers() {
         return ResponseEntity.ok(customerService.getAllCustomers());
     }
